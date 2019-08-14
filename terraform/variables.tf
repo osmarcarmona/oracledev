dacfeb3741fa9a9d4ae3d9e957a14a95e55183ed
cmosmar <imposible_no@hotmail.com>
2019-08-14 01:16:39 -0500

    new location

---
+++ terraform/variables.tf
@@ -0,0 +1,13 @@
+# AWS Config
+
+variable "aws_access_key" {
+  default = "AKIAINXP3O4W5KKMFTPQ"
+}
+
+variable "aws_secret_key" {
+  default = "QehNEhXoZ8JU/p5FbUkXsnl8J/904QfYLqRBPE1X"
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
