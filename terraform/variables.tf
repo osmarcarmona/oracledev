6a8b7249af783755fc7e74400776660c80cfadd8
imposible_no@hotmail.com <imposible_no@hotmail.com>
2019-08-18 18:45:07 -0500

    variable fix, using locals

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,11 +4,13 @@ variable "ssh_private_key" {
         description     = "Private key for us-east-1"
 }
 
-variable "aws_access_key" {
-}
+#variable "aws_access_key_env" {
+ #default = "${var.aws_access_key_env}"
+#}
 
-variable "aws_secret_key" {
-}
+#variable "aws_secret_access_key_env" {
+  #default = "${var.aws_secret_access_key_env}"
+#}
 
 variable "aws_region" {
   default = "us-east-1"
